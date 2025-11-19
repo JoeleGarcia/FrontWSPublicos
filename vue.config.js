bfafe935bebf25062bc798e536a610ce00929ec9
JoelGarcia <Joel.eGarcia@live.com>
2025-11-18 20:34:35 -0400

    config y api url

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 module.exports = {
   devServer: {
-    https: {
+    hhttps: process.env.NODE_ENV !== 'production' ? {
       key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem'))
-    },
+    } : false,
     proxy: {
       '^/api': {
-        target: 'https://localhost:7258',
+        target: 'https://wsapi.wslab.qzz.io',
         changeOrigin: true,
         secure: false,
         pathRewrite: { '^/api': '' }
